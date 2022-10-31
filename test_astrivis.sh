56e09efb778b756fcd1b4551da88d3593bb84be3
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-10-31 14:08:08 +0100

    adding equal signs

--- test_astrivis.sh
+++ test_astrivis.sh
@@ -1,4 +1,4 @@
 for i in 018 036 053 057 068 070 082 094 100 101 113 116 141 166 175
 do
-python3 shape_transfer_astrivis.py -s astrivis_data/model${i}/dense1.ply -t astrivis_data/model${i}/dense2.ply --config config/LNDP.yaml --directory astrivis_data/model${i}
+python3 shape_transfer_astrivis.py -s=astrivis_data/model${i}/dense1.ply -t=astrivis_data/model${i}/dense2.ply --config=config/LNDP.yaml --directory=astrivis_data/model${i}
 done
