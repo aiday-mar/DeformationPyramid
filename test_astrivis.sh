0a66fc4731dc48103d42bb6aeb44c48b07b94778
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-10-31 14:40:38 +0100

    adding an intermediate level

--- test_astrivis.sh
+++ test_astrivis.sh
@@ -1,4 +1,6 @@
 for i in 018 036 053 057 068 070 082 094 100 101 113 116 141 166 175
 do
-python3 shape_transfer_astrivis.py -s=astrivis_data/model${i}/dense1.ply -t=astrivis_data/model${i}/dense2.ply --config=config/LNDP.yaml --directory=astrivis_data/model${i}
+#folder='DynamicStrongMotion'
+folder='DynamicFlipMerge'
+python3 shape_transfer_astrivis.py -s=astrivis_data/${folder}/model${i}/dense1.ply -t=astrivis_data/${folder}/model${i}/dense2.ply --config=config/LNDP.yaml --directory=astrivis_data/${folder}/model${i}
 done
