996babbba0ab01d2044ed79dbf9c081a546ac425
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-01 10:05:17 +0100

    sampling in order to have smaller points

--- test_astrivis.sh
+++ test_astrivis.sh
@@ -3,7 +3,7 @@ for i in 207 210 211 215
 # for i in 036 053 057
 do
 #folder='DynamicStrongMotion'
-folder='DynamicFlipMerge'
+folder='DynamicFlipMergeSampled003'
 # folder='DynamicStrongMotionTransformed'
 python3 shape_transfer_astrivis.py -s=astrivis_data/${folder}/model${i}/dense1.ply -t=astrivis_data/${folder}/model${i}/dense2.ply --config=config/LNDP.yaml --directory=astrivis_data/${folder}/model${i}
 done
