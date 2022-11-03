513a79de6d9ee9f8a696666038698d1a9d24b068
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-03 16:46:11 +0100

    putting back transpose that used to be there

--- correspondence/datasets/dataloader.py
+++ correspondence/datasets/dataloader.py
@@ -558,7 +558,7 @@ def collate_fn_4dmatch(pairwise_data, config, neighborhood_limits ):
         modified_trans = batched_trn[entry_id].numpy()
         if modified_trans.ndim == 1:
             modified_trans = np.expand_dims(modified_trans, axis=0)
-        s_pc_wrapped = ( batched_rot[entry_id].numpy() @ c_src_pcd_deformed + modified_trans ).T
+        s_pc_wrapped = ( batched_rot[entry_id].numpy() @ c_src_pcd_deformed.T + modified_trans ).T
         coarse_match_gt = torch.from_numpy( multual_nn_correspondence(s_pc_wrapped , c_tgt_pcd_np , search_radius=config['coarse_match_radius'])  )# 0.1m scaled
         coarse_matches.append(coarse_match_gt)
         coarse_flow.append(torch.from_numpy(c_flow))
