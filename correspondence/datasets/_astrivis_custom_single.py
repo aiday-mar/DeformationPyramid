08c558e556e79d10edd5b25a0a4b85c844bcd126
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-08 19:58:27 +0100

    updating the test dataset with the correct code

--- correspondence/datasets/_astrivis_custom_single.py
+++ correspondence/datasets/_astrivis_custom_single.py
@@ -61,11 +61,14 @@ class _AstrivisCustomSingle(Dataset):
         src_trans_file=h5py.File(self.path + self.source_trans, "r")
         src_pcd_transform = np.array(src_trans_file['transformation'])
 
+        # transformations used differently
         tgt_trans_file=h5py.File(self.path + self.target_trans, "r")
         tgt_pcd_transform_inverse = np.linalg.inv(np.array(tgt_trans_file['transformation']))
         
         rot = np.matmul(tgt_pcd_transform_inverse[:3, :3], src_pcd_transform[:3, :3])
         trans = tgt_pcd_transform_inverse[:3, :3]@src_pcd_transform[:3, 3] + tgt_pcd_transform_inverse[:3, 3]
+        #
+        
         trans = np.expand_dims(trans, axis=0)
         trans = trans.transpose()
         
