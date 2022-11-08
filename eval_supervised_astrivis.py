e06309a87e7a1f27696db1e86d2a77d3bdfaf1f9
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-08 17:53:15 +0100

    opying the warped_pcd into the cpu before convertin to a numpy array

--- eval_supervised_astrivis.py
+++ eval_supervised_astrivis.py
@@ -119,7 +119,7 @@ if __name__ == "__main__":
         
         # warped_pcd is presumably the final pcd
         final_pcd = o3d.geometry.PointCloud()
-        final_pcd.points = o3d.utility.Vector3dVector(np.array(warped_pcd))
+        final_pcd.points = o3d.utility.Vector3dVector(np.array(warped_pcd.cpu()))
         o3d.io.write_point_cloud(args.output, final_pcd)
         
         flow = warped_pcd - model.src_pcd
