5ddbcda73536de21d769bba5d812711ed0938b98
aiday-mar <aidaymarlenkyzy@gmail.com>
2022-11-09 22:54:06 +0100

    updating the for loop in the bash script

--- test_astrivis_full_deformed.sh
+++ test_astrivis_full_deformed.sh
@@ -1,7 +1,6 @@
 base='home/aiday.kyzy/dataset/Synthetic/FullDeformedData/TestingData'
-# base='home/aiday.kyzy/dataset/Synthetic/FullNonDeformedData/TestingData'
 model_numbers=('002', '008', '015', '022', '029', '035', '042', '049', '056', '066', '073', '079', '085', '093', '100', '106', '113', '120', '126', '133', '140', '147', '153', '160', '167', '174', '180', '187', '194', '201', '207', '214', '221')
-for k in model_numbers
+for k in ${model_numbers[@]}
 do
 
 arr=('020', '041', '062', '104', '125', '146', '188', '209', '230')
