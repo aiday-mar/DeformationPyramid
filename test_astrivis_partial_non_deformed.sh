ff756a0ed3e1f48ec1f6dc37df19c32b11857d1d
aiday-mar <aidaymarlenkyzy@gmail.com>
2022-11-09 23:07:36 +0100

    modifying the files in order to creat the empty h5 file before populating it with data

--- test_astrivis_partial_non_deformed.sh
+++ test_astrivis_partial_non_deformed.sh
@@ -5,6 +5,7 @@ for k in ${model_numbers[@]}
 do
 
     mkdir $base/model$k/output
+    touch ${base}/model${k}/output/0_1_se4.h5
     python3 eval_supervised_astrivis.py --config=config/LNDP.yaml --s="PartialNonDeformedData/TestingData/model${k}/transformed/mesh_transformed_0.ply" --t="PartialNonDeformedData/TestingData/model${k}/transformed/mesh_transformed_1.ply" --source_trans="PartialNonDeformedData/TestingData/model${k}/transformed/mesh_transformed_0_se4.h5" --target_trans="PartialNonDeformedData/TestingData/model${k}/transformed/mesh_transformed_1_se4.h5" --matches="PartialNonDeformedData/TestingData/model${k}/matches/0_1.npz" --output="PartialNonDeformedData/TestingData/model${k}/output/0_1.ply" --output_trans="PartialNonDeformedData/TestingData/model${k}/output/0_1_se4.h5"
     python3 ../../../../code/sfm/python/graphics/mesh/compute_relative_transformation_error.py --part1="${base}/model${k}/transformed/mesh_transformed_0_se4.h5" --part2="${base}/model${k}/transformed/mesh_transformed_1_se4.h5" --pred="${base}/model${k}/output/0_1_se4.h5"
     python3 ../../../../code/sfm/python/graphics/mesh/compute_pointcloud_rmse_ir.py --final="${base}/model${k}/output/0_1.ply" --initial="${base}/model${k}/transformed/mesh_transformed_0.ply" --part1="${base}/model${k}/transformed/mesh_transformed_0_se4.h5" --part2="${base}/model${k}/transformed/mesh_transformed_1_se4.h5"
