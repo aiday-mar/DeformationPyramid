71656c3181f6112029b04922f02ba17b58883007
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-10 11:07:22 +0100

    ading file to do matching in the full case

--- test_astrivis_full_non_deformed.sh
+++ test_astrivis_full_non_deformed.sh
@@ -7,6 +7,6 @@ do
     touch ${base}/model${k}/output/0_1_se4.h5
     python3 eval_supervised_astrivis.py --config=config/LNDP.yaml --s="FullNonDeformedData/TestingData/model${k}/mesh_transformed_0.ply" --t="FullNonDeformedData/TestingData/model${k}/mesh_transformed_1.ply" --source_trans="FullNonDeformedData/TestingData/model${k}/mesh_transformed_0_se4.h5" --target_trans="FullNonDeformedData/TestingData/model${k}/mesh_transformed_1_se4.h5" --matches="FullNonDeformedData/TestingData/model${k}/0_1.npz" --output="FullNonDeformedData/TestingData/model${k}/output/0_1.ply" --output_trans="FullNonDeformedData/TestingData/model${k}/output/0_1_se4.h5"
     python3 ../../code/sfm/python/graphics/mesh/compute_relative_transformation_error.py --part1="${base}/model${k}/mesh_transformed_0_se4.h5" --part2="${base}/model${k}/mesh_transformed_1_se4.h5" --pred="${base}/model${k}/output/0_1_se4.h5"
-    python3 ../../code/sfm/python/graphics/mesh/compute_pointcloud_rmse_ir.py --input1="${base}/model${k}/output/0_1.ply" --input2="${base}/model${k}/mesh_transformed_1.ply"
+    python3 ../../code/sfm/python/graphics/mesh/compute_pointcloud_rmse_ir.py --input1="${base}/model${k}/output/0_1.ply" --input2="${base}/model${k}/mesh_transformed_1.ply" --matches="FullNonDeformedData/TestingData/model${k}/0_1.npz"
 
 done
