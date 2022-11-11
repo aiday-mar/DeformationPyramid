0940eadccd05647e3f78346a5fbe39eb93279052
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-11 17:51:56 +0100

    changed the sh files

--- test_astrivis_full_non_deformed.sh
+++ test_astrivis_full_non_deformed.sh
@@ -6,8 +6,10 @@ do
     mkdir $base/model$k/output
     touch ${base}/model${k}/output/0_1_se4.h5
     echo "model ${k}"
-    python3 eval_supervised_astrivis.py --config=config/LNDP.yaml --s="FullNonDeformedData/TestingData/model${k}/mesh_transformed_0.ply" --t="FullNonDeformedData/TestingData/model${k}/mesh_transformed_1.ply" --source_trans="FullNonDeformedData/TestingData/model${k}/mesh_transformed_0_se4.h5" --target_trans="FullNonDeformedData/TestingData/model${k}/mesh_transformed_1_se4.h5" --matches="FullNonDeformedData/TestingData/model${k}/0_1.npz" --output="FullNonDeformedData/TestingData/model${k}/output/0_1.ply" --output_trans="FullNonDeformedData/TestingData/model${k}/output/0_1_se4.h5"
-    python3 ../../code/sfm/python/graphics/mesh/compute_relative_transformation_error.py --part1="${base}/model${k}/mesh_transformed_0_se4.h5" --part2="${base}/model${k}/mesh_transformed_1_se4.h5" --pred="${base}/model${k}/output/0_1_se4.h5"
-    python3 ../../code/sfm/python/graphics/mesh/compute_pointcloud_rmse_ir.py --input1="${base}/model${k}/output/0_1.ply" --input2="${base}/model${k}/mesh_transformed_1.ply" --matches="${base}/model${k}/0_1.npz"
+    python3 eval_supervised_astrivis.py --config=config/LNDP.yaml --s="FullNonDeformedData/TestingData/model${k}/mesh_transformed_0.ply" --t="FullNonDeformedData/TestingData/model${k}/mesh_sampled.ply" --source_trans="FullNonDeformedData/TestingData/model${k}/mesh_transformed_0_se4.h5" --target_trans="identity.h5" --matches="FullNonDeformedData/TestingData/model${k}/0_1.npz" --output="FullNonDeformedData/TestingData/model${k}/output/0_1.ply" --output_trans="FullNonDeformedData/TestingData/model${k}/output/0_1_se4.h5" --intermediate_ouput_folder="FullNonDeformedData/TestingData/model${k}/output/"
+    
+    python3 ../../code/sfm/python/graphics/mesh/compute_relative_transformation_error.py --part1="${base}/model${k}/mesh_transformed_0_se4.h5" --part2="identity.h5" --pred="${base}/model${k}/output/0_1_se4.h5"
+    
+    python3 ../../code/sfm/python/graphics/mesh/compute_pointcloud_rmse_ir.py --input1="${base}/model${k}/output/0_1.ply" --input2="${base}/model${k}/mesh_sampled.ply" --matches="${base}/model${k}/0_1.npz"
 
 done
