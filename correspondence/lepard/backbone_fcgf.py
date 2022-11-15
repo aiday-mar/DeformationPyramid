10cc783f532a2b7ddbd4d9e45130a1be831374b2
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-15 15:09:58 +0100

    changed 528 to 32

--- correspondence/lepard/backbone_fcgf.py
+++ correspondence/lepard/backbone_fcgf.py
@@ -15,14 +15,13 @@ class FCGF(nn.Module):
         print('Inside of FCGF forward')
         source_pcd = batch['src_pcd_list'][0]
         target_pcd = batch['tgt_pcd_list'][0]
-        print(source_pcd)
-        print(target_pcd)
         src_pcd = o3d.geometry.PointCloud()
         src_pcd.points = o3d.utility.Vector3dVector(np.array(source_pcd.cpu()))
         o3d.io.write_point_cloud('src_pcd.ply', src_pcd)
         tgt_pcd = o3d.geometry.PointCloud()
         tgt_pcd.points = o3d.utility.Vector3dVector(np.array(target_pcd.cpu()))
         o3d.io.write_point_cloud('tgt_pcd.ply', tgt_pcd)
+        
         # TODO: not possible to easily change conda environments
         '''
         command = 'conda init bash'
@@ -31,15 +30,11 @@ class FCGF(nn.Module):
         command = 'conda activate py3-fcgf-3'
         os.system(command)
         '''
-        
         # subprocess.run('conda init bash & conda activate py3-fcgf-3 && python3 ../sfm/python/vision/features/feature_fcgf_cli.py --input="src_pcd.ply" --input="src_pcd.npz" && conda deactivate', shell=True)
-
         # command = 'python3 ../sfm/python/vision/features/feature_fcgf_cli.py --input="src_pcd.ply" --input="src_pcd.npz"'
         # os.system(command)
-        
         # command = 'python3 ../sfm/python/vision/features/feature_fcgf_cli.py --input="tgt_pcd.ply" --input="tgt_pcd.npz"'
         # os.system(command)
-        
         '''
         command = 'conda deactivate'
         os.system(command)
