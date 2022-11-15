99b38c357bf7cbecc4646fb62d3bb0fe5e67ccc7
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-15 17:37:49 +0100

    adding base in front

--- correspondence/lepard/backbone_fcgf.py
+++ correspondence/lepard/backbone_fcgf.py
@@ -11,15 +11,15 @@ class FCGF(nn.Module):
     def __init__(self, config):
         super(FCGF, self).__init__()
 
-    def forward(self, batch, s_feats = None, t_feats = None, phase = 'encode'):
+    def forward(self, batch, s_feats = None, t_feats = None, base = None, phase = 'encode'):
         
         features_src = None
         features_tgt = None
         
         # TODO: Use pregenerated features
         if s_feats and t_feats:
-            features_src = np.load(s_feats)
-            features_tgt = np.load(t_feats)
+            features_src = np.load(base + s_feats)
+            features_tgt = np.load(base + t_feats)
         
             features_src = features_src['arr_0']
             features_tgt = features_tgt['arr_0']
