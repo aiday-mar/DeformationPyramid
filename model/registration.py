f3db3f957e500dd22ff9469ccad3e30dbbd468df
aiday-mar <aidaymarlenkyzy@gmail.com>
2022-11-16 23:12:20 +0100

    not pringint the shape of tuple

--- model/registration.py
+++ model/registration.py
@@ -249,7 +249,7 @@ class Registration():
                 # without removing the translation, so we can see better the result
                 print('level : ', level)
                 print('data.keys() : ', data.keys())
-                print(data[level].shape)
+                print(data[level])
                 # src_mask = data['src_mask']
                 # tgt_mask = data['tgt_mask']
                 print(warped_ldmk.shape)
