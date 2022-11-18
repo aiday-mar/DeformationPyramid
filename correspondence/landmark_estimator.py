68dec745ea6ae645d625be1b5554f5e844211832
aiday-mar <aidaymarlenkyzy@gmail.com>
2022-11-18 20:32:45 +0100

    converting with float

--- correspondence/landmark_estimator.py
+++ correspondence/landmark_estimator.py
@@ -251,7 +251,7 @@ class Landmark_Model():
                         UV_det = np.linalg.det(U) * np.linalg.det(V)
                         S[2, 2] = UV_det
                         sv = np.matmul( S, V )
-                        R = np.matmul( U, sv).float()
+                        R = np.matmul( U, sv)
                         t = mean_Y.T - np.matmul( R, mean_X.T )
                         
                         if print_size:
