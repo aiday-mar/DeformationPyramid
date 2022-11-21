4cb9042b1aa5a48b58641ac791312b2b50f95e51
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-21 12:47:52 +0100

    printing the outleirs in the end

--- correspondence/landmark_estimator.py
+++ correspondence/landmark_estimator.py
@@ -301,7 +301,8 @@ class Landmark_Model():
                             
                     print_size = False
 
-                outliers = dict((k, v) for k, v in outliers.items() if v >= 5)
+                outliers = dict((k, v) for k, v in outliers.items() if v >= 6)
+                print('outliers : ', outliers)
                 total_outliers = set(outliers.keys())                                  
                 final_indices = np.array([i for i in range(0, len(ldmk_s_np)) if i not in total_outliers])
                 ldmk_s = torch.tensor(ldmk_s_np[final_indices]).to('cuda:0')
