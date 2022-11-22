f0d47ce392f14f591a3c953a3b726c59d1e28b84
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-22 12:47:21 +0100

    removing the folder before making it again

--- testing_different_w_print_keypoints.sh
+++ testing_different_w_print_keypoints.sh
@@ -20,9 +20,13 @@ coarse_level=-2
 index_coarse_feats=1
 # index_coarse_feats=2
 
+rm -rf TestData/FullNonDeformed/output_type_${type}_preprocessing_${preprocessing}_confidence_${confidence}_coarse_level_${coarse_level}_index_coarse_${index_coarse_feats}
 mkdir TestData/FullNonDeformed/output_type_${type}_preprocessing_${preprocessing}_confidence_${confidence}_coarse_level_${coarse_level}_index_coarse_${index_coarse_feats}
+rm -rf TestData/FullDeformed/output_type_${type}_preprocessing_${preprocessing}_confidence_${confidence}_coarse_level_${coarse_level}_index_coarse_${index_coarse_feats}
 mkdir TestData/FullDeformed/output_type_${type}_preprocessing_${preprocessing}_confidence_${confidence}_coarse_level_${coarse_level}_index_coarse_${index_coarse_feats}
+rm -rf TestData/PartialNonDeformed/output_type_${type}_preprocessing_${preprocessing}_confidence_${confidence}_coarse_level_${coarse_level}_index_coarse_${index_coarse_feats}
 mkdir TestData/PartialNonDeformed/output_type_${type}_preprocessing_${preprocessing}_confidence_${confidence}_coarse_level_${coarse_level}_index_coarse_${index_coarse_feats}
+rm -rf TestData/PartialDeformed/output_type_${type}_preprocessing_${preprocessing}_confidence_${confidence}_coarse_level_${coarse_level}_index_coarse_${index_coarse_feats}
 mkdir TestData/PartialDeformed/output_type_${type}_preprocessing_${preprocessing}_confidence_${confidence}_coarse_level_${coarse_level}_index_coarse_${index_coarse_feats}
 
 file_name=TestData/type_${type}_preprocessing_${preprocessing}_confidence_${confidence}_coarse_level_${coarse_level}_index_coarse_${index_coarse_feats}.txt
