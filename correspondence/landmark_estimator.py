93d0f27e127cb5276b470a6b1ceb349fb5fa3818
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-23 14:55:42 +0100

    increasing the number of columns to 3 from 2

--- correspondence/landmark_estimator.py
+++ correspondence/landmark_estimator.py
@@ -779,7 +779,7 @@ class Landmark_Model():
                         total_points = np.concatenate((ldmk_s_np, ldmk_t_np), axis = 0)
                         number_points_src = ldmk_s_np.shape[0]
                         correspondences = np.array([[i, i + number_points_src] for i in range(0, number_points_src)])
-                        colors = np.zeros((correspondences.shape[0], correspondences.shape[1]))
+                        colors = np.zeros((correspondences.shape[0], 3))
                         colors[point_indices_close_to_center[final_inliers]] = np.array([0.7, 0.7, 0.7])
                         line_set = o3d.geometry.LineSet()
                         line_set.points=o3d.utility.Vector3dVector(total_points)
