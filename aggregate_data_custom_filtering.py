4d3aeb9b0c36b7cdd1125e4cf67b8889e75fc3b0
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-23 10:33:01 +0100

    printing the final matrices

--- aggregate_data_custom_filtering.py
+++ aggregate_data_custom_filtering.py
@@ -1,5 +1,6 @@
 import re
 import matplotlib.pyplot as plt
+import numpy as np
 
 class File:
     def __init__(self, type, preprocessing, confidence, number_centers, average_distance_multiplier, coarse_level, index_coarse, number_iterations):
@@ -15,52 +16,36 @@ class File:
     def __str__(self):
         return " - Type : " + str(self.type) + " - Preprocessing : " + str(self.preprocessing) + " - Confidence : " + str(self.confidence) + " - Number Centers : " + str(self.number_centers) + " - Average Distance Multiplier : " + str(self.average_distance_multiplier) + " - Coarse Level : " + str(self.coarse_level) + " - Index Coarse : " + str(self.index_coarse) + ' - Number Iterations : ' + self.number_iterations
 
-# Version 1 files -OLD
-'''        
-files=[
-    "version_1_type_custom_preprocessing_mutual_confidence_0.1_number_centers_1000_coarse_level_-2_index_coarse_1.txt", 
-    "version_1_type_custom_preprocessing_none_confidence_0.1_number_centers_1000_coarse_level_-2_index_coarse_1.txt", 
-    "version_1_type_custom_preprocessing_none_confidence_0.1_number_centers_1000_coarse_level_-3_index_coarse_1.txt", 
-    "version_1_type_custom_preprocessing_none_confidence_0.1_number_centers_1000_coarse_level_-3_index_coarse_2.txt",
-    "version_1_type_custom_preprocessing_none_confidence_0.05_number_centers_1000_coarse_level_-2_index_coarse_1.txt",
-    "version_1_type_default_preprocessing_mutual_confidence_0.1_number_centers_1000_coarse_level_-2_index_coarse_1.txt"
-]
-
-file_types=[
-    File('custom', 'mutual', 0.1, -2, 1),
-    File('custom', 'none', 0.1, -2, 1),
-    File('custom', 'none', 0.1, -3, 1),
-    File('custom', 'none', 0.1, -3, 2),
-    File('custom', 'none', 0.05, -2, 1),
-    File('default', 'mutual', 0.1, -2, 1)
-]
-'''
 
 # Version 2 files
 files=[]
 file_types=[]
 number_iterations=1
-
-for i in [100, 200, 300, 400]:
-    for j in [1, 2, 3, 4]:
-        files.append('v_2_t_custom_p_none_c_0.1_nc_' + str(i) + '_adm_' + str(j) + '_cl_-2_ic_1_ni_' + str(number_iterations) +'.txt')
-        file_types.append(File('custom', 'none', 0.1, i, j, -2, 1, number_iterations))
-
 data_types=['Full Non Deformed', 'Full Deformed', 'Partial Deformed', 'Partial Non Deformed']
 base = 'TestData/'
 final_data = {}
-for file in files:
-    final_data[file] = {}
-    file_txt = open(base + file, 'r')
-    Lines = file_txt.readlines()
-    current_data_type = None
-    for line in Lines:
-        if line[:-1] in data_types:
-            current_data_type = line[:-1]
-            final_data[file][current_data_type] = {}
-                    
-        if 'RMSE' in line:
-            final_data[file][current_data_type]['RMSE'] = float(re.findall("\d+\.\d+", line)[0])
+final_matrices={'Full Non Deformed': np.zeros((3, 4)), 'Full Non Deformed': np.zeros((3, 4)), 'Partial Deformed': np.zeros((3, 4)),  'Partial Non Deformed': np.zeros((3, 4))}
+
+for i in [100, 200, 300]:
+    for j in [1, 2, 3, 4]:
+        file = 'v_2_t_custom_p_none_c_0.1_nc_' + str(i) + '_adm_' + str(j) + '_cl_-2_ic_1_ni_' + str(number_iterations) +'.txt'
+        files.append(file)
+        file_types.append(File('custom', 'none', 0.1, i, j, -2, 1, number_iterations))
+        final_data[file] = {}
+        file_txt = open(base + file, 'r')
+        Lines = file_txt.readlines()
+        current_data_type = ''
+        for line in Lines:
+            if line[:-1] in data_types:
+                current_data_type = line[:-1]
+                final_data[file][current_data_type] = {}
+                        
+            if 'RMSE' in line:
+                rmse = float(re.findall("\d+\.\d+", line)[0])
+                final_data[file][current_data_type]['RMSE'] = rmse
+                final_matrices[current_data_type][i][j] = rmse
+
+print('final_matrices : ', final_matrices)
 
 RMSE_full_deformed = []
 RMSE_full_non_deformed = []
