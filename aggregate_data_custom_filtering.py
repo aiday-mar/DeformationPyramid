16cd7b9e6a7091c63a4d8541091404f170d649ed
aiday-mar <aidaymarlenkyzy@gmail.com>
2022-11-23 19:42:11 +0100

    consideiring the case when heatmap is true or not

--- aggregate_data_custom_filtering.py
+++ aggregate_data_custom_filtering.py
@@ -17,23 +17,31 @@ class File:
     def __str__(self):
         return " - Type : " + str(self.type) + " - Preprocessing : " + str(self.preprocessing) + " - Confidence : " + str(self.confidence) + " - Number Centers : " + str(self.number_centers) + " - Average Distance Multiplier : " + str(self.average_distance_multiplier) + " - Coarse Level : " + str(self.coarse_level) + " - Index Coarse : " + str(self.index_coarse) + ' - Number Iterations : ' + str(self.number_iterations)
 
-
 # Version 2 files
 files=[]
 file_types=[]
 number_iterations=1
 data_types=['Full Non Deformed', 'Full Deformed', 'Partial Deformed', 'Partial Non Deformed']
 base = 'TestData/'
-final_data = {}
 
 # For heatmap
-# shape=(3,4)
-shape=(6,)
-final_matrices={'Full Non Deformed': np.zeros(shape), 'Full Deformed': np.zeros(shape), 'Partial Deformed': np.zeros(shape),  'Partial Non Deformed': np.zeros(shape)}
+# nc = [100, 200, 300]:
+nc = [20]
 
-for i in [20]: # [100, 200, 300]:
+# radius adm
+# adm = [1, 2, 3, 4]
+adm =  [1.0, 1.4, 1.8, 2.2, 2.6, 3.0]
+
+# heatmap = True
+heatmap = False
+
+shape=(len(nc), len(adm))
+final_matrices={'Full Non Deformed': np.zeros(shape), 'Full Deformed': np.zeros(shape), 'Partial Deformed': np.zeros(shape),  'Partial Non Deformed': np.zeros(shape)}
+final_data = {}
+
+for i in nc : 
     count = 0
-    for j in [1.0, 1.4, 1.8, 2.2, 2.6, 3.0]: # [1, 2, 3, 4]
+    for j in adm:
         file = 'v_2_t_custom_p_none_c_0.1_nc_' + str(i) + '_adm_' + str(j) + '_cl_-2_ic_1_ni_' + str(number_iterations) +'.txt'
         files.append(file)
         file_types.append(File('custom', 'none', 0.1, i, j, -2, 1, number_iterations))
@@ -46,16 +54,12 @@ for i in [20]: # [100, 200, 300]:
                 current_data_type = line[:-1]
                 final_data[file][current_data_type] = {}
 
-            # Use when matrix obtained for heatmap
-            '''
-            if 'RMSE' in line:
+            if heatmap and 'RMSE' in line:
                 rmse = float(re.findall("\d+\.\d+", line)[0])
                 final_data[file][current_data_type]['RMSE'] = rmse
                 final_matrices[current_data_type][int(i/100)-1][j-1] = rmse
-            '''
 
-            # For graph
-            if 'RMSE' in line:
+            if not heatmap and 'RMSE' in line:
                 rmse = float(re.findall("\d+\.\d+", line)[0])
                 final_data[file][current_data_type]['RMSE'] = rmse
                 final_matrices[current_data_type][count] = rmse
@@ -66,16 +70,16 @@ print('final_data : ', final_data)
 print('final_matrices : ', final_matrices)
 
 # Heatmap
-'''
-for data_type in data_types:
-    ax = sns.heatmap(final_matrices[data_type], linewidth=0.5)
-    figure = ax.get_figure()    
-    figure.savefig('plots/custom_filtering_v2/' + data_type.replace(' ', '_') + '.png', dpi=400)
-'''
+if heatmap:
+    for data_type in data_types:
+        ax = sns.heatmap(final_matrices[data_type], linewidth=0.5)
+        figure = ax.get_figure()    
+        figure.savefig('plots/custom_filtering_v2/' + data_type.replace(' ', '_') + '.png', dpi=400)
 
-for data_type in data_types:
-    plt.plot(final_matrices[data_type])
-    plt.savefig('plots/custom_filtering_v2/' + data_type.replace(' ', '_') + '_varying_radius_' + '.png')
+if not heatmap:
+    for data_type in data_types:
+        plt.plot(final_matrices[data_type])
+        plt.savefig('plots/custom_filtering_v2/' + data_type.replace(' ', '_') + '_varying_radius_' + '.png')
 
 RMSE_full_deformed = []
 RMSE_full_non_deformed = []
