4520cb8970cbd696e92fb78ee21231dd031d7971
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-24 13:07:36 +0100

    simplifying the code dont need all the rest of the code

--- aggregate_data_custom_filtering_true_correspondence_ratio.py
+++ aggregate_data_custom_filtering_true_correspondence_ratio.py
@@ -31,7 +31,6 @@ adm =  [1.0, 1.4, 1.8, 2.2, 2.6, 3.0, 3.4, 3.8, 4.2, 4.6, 5.0]
 
 shape=(len(nc), len(adm))
 final_matrices={'Full Non Deformed': np.zeros(shape), 'Full Deformed': np.zeros(shape), 'Partial Deformed': np.zeros(shape),  'Partial Non Deformed': np.zeros(shape)}
-final_data = {}
 
 for i in nc : 
     count = 0
@@ -39,18 +38,15 @@ for i in nc :
         file = 'v_2_t_custom_p_none_c_0.1_nc_' + str(i) + '_adm_' + str(j) + '_cl_-2_ic_1_ni_' + str(number_iterations) +'.txt'
         files.append(file)
         file_types.append(File('custom', 'none', 0.1, i, j, -2, 1, number_iterations))
-        final_data[file] = {}
         file_txt = open(base + file, 'r')
         Lines = file_txt.readlines()
         current_data_type = ''
         for line in Lines:
             if line[:-1] in data_types:
                 current_data_type = line[:-1]
-                final_data[file][current_data_type] = {}
 
             if 'RMSE' in line:
                 rmse = float(re.findall("\d+\.\d+", line)[0])
-                final_data[file][current_data_type]['RMSE'] = rmse
                 final_matrices[current_data_type][0][count] = rmse
         
         count += 1
@@ -59,54 +55,3 @@ for data_type in data_types:
     plt.plot(adm, np.squeeze(final_matrices[data_type].T))
     plt.legend(data_types)
     plt.savefig('plots/custom_filtering_v4/true_correspondence_ratio_for_varying_radius.png')
-
-RMSE_full_deformed = []
-RMSE_full_non_deformed = []
-RMSE_partial_deformed = []
-RMSE_partial_non_deformed = []
-
-for file in files:
-    RMSE_full_deformed.append(final_data[file]['Full Deformed']['RMSE'])
-    RMSE_full_non_deformed.append(final_data[file]['Full Non Deformed']['RMSE'])
-    RMSE_partial_deformed.append(final_data[file]['Partial Deformed']['RMSE'])
-    RMSE_partial_non_deformed.append(final_data[file]['Partial Non Deformed']['RMSE'])
-
-fig = plt.figure(figsize = (10, 5))
-plt.bar(files, RMSE_full_deformed, width = 0.4)
-plt.ylabel("RMSE")
-plt.title("RMSE - Full Deformed")
-plt.savefig('plots/custom_filtering_v2/RMSE_Full_Deformed_Custom_Filtering.png')
-
-fig = plt.figure(figsize = (10, 5))
-plt.bar(files, RMSE_full_non_deformed, width = 0.4)
-plt.ylabel("RMSE")
-plt.title("RMSE - Full Non Deformed")
-plt.savefig('plots/custom_filtering_v2/RMSE_Full_Non_Deformed_Custom_Filtering.png')
-
-fig = plt.figure(figsize = (10, 5))
-plt.bar(files, RMSE_partial_deformed, width = 0.4)
-plt.ylabel("RMSE")
-plt.title("RMSE - Partial Deformed")
-plt.savefig('plots/custom_filtering_v2/RMSE_Partial_Deformed_Custom_Filtering.png')
-
-fig = plt.figure(figsize = (10, 5))
-plt.bar(files, RMSE_partial_non_deformed, width = 0.4)
-plt.ylabel("RMSE")
-plt.title("RMSE - Partial Non Deformed")
-plt.savefig('plots/custom_filtering_v2/RMSE_Partial_Non_Deformed_Custom_Filtering.png')
-
-print('RMSE_full_deformed : ', RMSE_full_deformed)
-RMSE_full_deformed_index = RMSE_full_deformed.index(min(RMSE_full_deformed))
-print('Minimum attained for : ', file_types[RMSE_full_deformed_index])
-
-print('RMSE_full_non_deformed : ', RMSE_full_non_deformed)
-RMSE_full_non_deformed_index = RMSE_full_non_deformed.index(min(RMSE_full_non_deformed))
-print('Minimum attained for : ', file_types[RMSE_full_non_deformed_index])
-
-print('RMSE_partial_deformed : ', RMSE_partial_deformed)
-RMSE_partial_deformed_index = RMSE_partial_deformed.index(min(RMSE_partial_deformed))
-print('Minimum attained for : ', file_types[RMSE_partial_deformed_index])
-
-print('RMSE_partial_non_deformed : ', RMSE_partial_non_deformed)
-RMSE_partial_non_deformed_index = RMSE_partial_non_deformed.index(min(RMSE_partial_non_deformed))
-print('Minimum attained for : ', file_types[RMSE_partial_non_deformed_index])
