852bf3f5266b96664e06774bbaf2079575cfe60f
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-25 18:04:51 +0100

    using poisson sampling

--- aggregate_data_custom_filtering_true_correspondence_ratio.py
+++ aggregate_data_custom_filtering_true_correspondence_ratio.py
@@ -35,8 +35,8 @@ adm = [3.0]
 # iot=[0.005, 0.01, 0.02, 0.03, 0.04, 0.05]
 iot = [0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01, 0.02, 0.03, 0.04, 0.05]
 
-# sampling = 'poisson'
-sampling='linspace'
+sampling = 'poisson'
+# sampling='linspace'
 
 version=4
 
