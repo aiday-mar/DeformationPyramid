0fc61811db10559125d17bae2297214875d16e69
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-25 13:22:30 +0100

    changing the data aggregation python file

--- aggregate_data_custom_filtering_true_correspondence_ratio.py
+++ aggregate_data_custom_filtering_true_correspondence_ratio.py
@@ -26,13 +26,14 @@ data_types=['Full Non Deformed', 'Full Deformed', 'Partial Deformed', 'Partial N
 base = 'TestData/'
 
 # nc = [100, 200, 300]:
-nc = [20]
+nc = [50]
 
 # adm = [1, 2, 3, 4]
 # adm =  [1.0, 1.4, 1.8, 2.2, 2.6, 3.0, 3.4, 3.8, 4.2, 4.6, 5.0]
 adm = [3.0]
 
-iot=[0.005, 0.01, 0.02, 0.03, 0.04, 0.05]
+# iot=[0.005, 0.01, 0.02, 0.03, 0.04, 0.05]
+iot = [0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01, 0.02, 0.03, 0.04, 0.05]
 
 # sampling = 'poisson'
 sampling='linspace'
