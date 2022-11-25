16d2d4e0abdfd46c88efd2c169e1460878e94950
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-25 12:26:56 +0100

    adding the second parameter returned from mesh poisson creation

--- correspondence/landmark_estimator.py
+++ correspondence/landmark_estimator.py
@@ -817,7 +817,7 @@ class Landmark_Model():
                     centers_pcd.points = o3d.utility.Vector3dVector(centers_points)
                 elif sampling == 'poisson':
                     ldmk_s_pcd.estimate_normals()
-                    ldmk_s_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(ldmk_s_pcd, depth=9)
+                    ldmk_s_mesh, _ = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(ldmk_s_pcd, depth=9)
                     centers_pcd = ldmk_s_mesh.sample_points_poisson_disk(number_of_points=number_centers) # pcl=ldmk_s_pcd)
                 
                 o3d.io.write_point_cloud(self.path + intermediate_output_folder + 'custom_filtering_ldmk/centers_pcd.ply', centers_pcd)
