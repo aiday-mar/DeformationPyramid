7c39e004d06b0b5b0cf800e68c41df78ba31d1c5
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-25 12:23:41 +0100

    estinating the normals on the ldmk_s_pcd

--- correspondence/landmark_estimator.py
+++ correspondence/landmark_estimator.py
@@ -816,6 +816,7 @@ class Landmark_Model():
                     centers_points = ldmk_s_np[neighborhood_center_indices_list]
                     centers_pcd.points = o3d.utility.Vector3dVector(centers_points)
                 elif sampling == 'poisson':
+                    ldmk_s_pcd.estimate_normals()
                     ldmk_s_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(ldmk_s_pcd, depth=9)
                     centers_pcd = ldmk_s_mesh.sample_points_poisson_disk(number_of_points=number_centers) # pcl=ldmk_s_pcd)
                 
