f3f4f552261ff7f45bf6bf2f23f8796d3c3b10a5
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-28 18:05:12 +0100

    printing the positional encoding function

--- model/nets.py
+++ model/nets.py
@@ -163,7 +163,7 @@ class NDPLayer(nn.Module):
     def posenc(self, pos, posenc_function = None):
         x_position, y_position, z_position = pos[..., 0:1], pos[..., 1:2], pos[..., 2:3]
         mul_term = None
-
+        print('positional encoding function : ', posenc_function)
         if not posenc_function or posenc_function == 'none':
             mul_term = (2 ** (self.m + self.k0))
         if posenc_function == 'power4':
