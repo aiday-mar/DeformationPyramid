925fb829ce993e1c28d684a61c42c1028a2979c7
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-28 18:13:29 +0100

    removing the print statement

--- model/nets.py
+++ model/nets.py
@@ -163,7 +163,6 @@ class NDPLayer(nn.Module):
     def posenc(self, pos, posenc_function = None):
         x_position, y_position, z_position = pos[..., 0:1], pos[..., 1:2], pos[..., 2:3]
         mul_term = None
-        print(posenc_function)
         if not posenc_function or posenc_function == 'none':
             mul_term = (2 ** (self.m + self.k0))
         elif 'power4' in posenc_function:
