be8dc0ebddc4e990677b02a9697231bd447ba87e
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-11-29 12:58:13 +0100

    changed the paths where the data is located for the training

--- correspondence/datasets/_astrivis_custom.py
+++ correspondence/datasets/_astrivis_custom.py
@@ -30,12 +30,13 @@ class _AstrivisCustom(Dataset):
         
         path = ''
         if self.split == 'train':
-            path = '/home/aiday.kyzy/dataset/TrainingDataDeformedFinal'
+            path = '/home/aiday.kyzy/dataset/Synthetic/PartialDeformedData/TrainingData/'
         elif self.split == 'val':
-            path = '/home/aiday.kyzy/dataset/ValidationDataDeformedFinal'
+            path = '/home/aiday.kyzy/dataset/Synthetic/PartialDeformedData/ValidationData/'
         elif self.split == 'test':
-            path = '/home/aiday.kyzy/dataset/ValidationDataDeformedFinal'
-            
+            path = '/home/aiday.kyzy/dataset/Synthetic/PartialDeformedData/TestingData/'
+        
+        self.path = path
         for folder in os.listdir(path):
             self.matches[folder] = []
             for filename in os.listdir(path + '/' + folder + '/matches'):
@@ -47,6 +48,7 @@ class _AstrivisCustom(Dataset):
             n_files_per_folder_found = True
     
         print('self.n_files_per_folder : ', self.n_files_per_folder)
+        print('number of matches : ', self.number_matches)
         print('number folders : ', len(self.matches))
         
     def __len__(self):
@@ -68,15 +70,15 @@ class _AstrivisCustom(Dataset):
         src_pcd_file = file_pointers[0] + '_' + file_pointers[2] + '.ply'
         tgt_pcd_file = file_pointers[1] + '_' + file_pointers[3] + '.ply'
         
-        src_pcd = o3d.io.read_point_cloud('/home/aiday.kyzy/dataset/TrainingDataDeformedFinal/' + folder_string + '/transformed/' + src_pcd_file)
+        src_pcd = o3d.io.read_point_cloud(self.path + folder_string + '/transformed/' + src_pcd_file)
         src_pcd = np.array(src_pcd.points)
-        tgt_pcd = o3d.io.read_point_cloud('/home/aiday.kyzy/dataset/TrainingDataDeformedFinal/' + folder_string + '/transformed/' + tgt_pcd_file)
+        tgt_pcd = o3d.io.read_point_cloud(self.path + folder_string + '/transformed/' + tgt_pcd_file)
         tgt_pcd = np.array(tgt_pcd.points)
         
         src_feats = np.ones_like(src_pcd[:, :1]).astype(np.float32)
         tgt_feats = np.ones_like(tgt_pcd[:, :1]).astype(np.float32)
         
-        matches = np.load('/home/aiday.kyzy/dataset/TrainingDataDeformedFinal/' + folder_string + '/matches/' + filename)
+        matches = np.load(self.path + folder_string + '/matches/' + filename)
         correspondences = np.array(matches['matches'])
         indices_src = correspondences[:, 0]
         indices_tgt = correspondences[:, 1]
@@ -93,10 +95,10 @@ class _AstrivisCustom(Dataset):
         src_pcd_trans = file_pointers[0] + '_' + file_pointers[2] + '_se4.h5'
         tgt_pcd_trans = file_pointers[1] + '_' + file_pointers[3] + '_se4.h5'
         
-        src_trans_file=h5py.File('/home/aiday.kyzy/dataset/TrainingDataDeformedFinal/' + folder_string + '/transformed/' + src_pcd_trans, "r")
+        src_trans_file=h5py.File(self.path + folder_string + '/transformed/' + src_pcd_trans, "r")
         src_pcd_transform = np.array(src_trans_file['transformation'])
         
-        tgt_trans_file=h5py.File('/home/aiday.kyzy/dataset/TrainingDataDeformedFinal/' + folder_string + '/transformed/' + tgt_pcd_trans, "r")
+        tgt_trans_file=h5py.File(self.path + folder_string + '/transformed/' + tgt_pcd_trans, "r")
         tgt_pcd_transform_inverse = np.linalg.inv(np.array(tgt_trans_file['transformation']))
         
         rot = np.matmul(tgt_pcd_transform_inverse[:3, :3], src_pcd_transform[:3, :3])
