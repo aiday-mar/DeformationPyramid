9b4fe46c31fb96ced3f71eebf45e868c1038889d
aiday-mar <aidaymarlenkyzy@gmail.com>
2022-12-04 21:12:48 +0100

    ading the possibility to change the config file and thus using a different configuration

--- Testing/all/test_astrivis_partial_deformed.sh
+++ Testing/all/test_astrivis_partial_deformed.sh
@@ -1,3 +1,9 @@
+# config=LNDP_fcgf.yaml
+config=LNDP.yaml
+filename=Testing/all/test_astrivis_partial_deformed.txt
+rm ${filename}
+touch ${filename}
+
 base='/home/aiday.kyzy/dataset/Synthetic/PartialDeformedData/TestingData'
 
 # model_numbers=('002' '008' '015' '022' '029' '035' '042' '049' '056' '066' '073' '079' '085' '093' '100' '106' '113' '120' '126' '133' '140' '147' '153' '160' '167' '174' '180' '187' '194' '201' '207' '214' '221')
@@ -20,20 +26,20 @@ for i in $(seq 0 $end); do
 		file_number1=${arr[$i]}
 		file_number2=${arr[$j]}
 		mkdir $base/model$k/output/${file_number1}_${file_number2}
-		echo "model ${k} i ${i} j ${j}"
+		echo "model ${k} i ${i} j ${j}" >> ${filename}
 		
 		# 0 -> 1
 		touch ${base}/model${k}/output/${file_number1}_${file_number2}_0_1_se4.h5
-		python3 eval_supervised_astrivis.py --config=config/LNDP.yaml --s="PartialDeformedData/TestingData/model${k}/transformed/${file_number1}_0.ply" --t="PartialDeformedData/TestingData/model${k}/temp_${file_number2}/model_1/cloud/dense.ply" --source_trans="PartialDeformedData/TestingData/model${k}/transformed/${file_number1}_0_se4.h5" --target_trans="identity.h5" --matches="PartialDeformedData/TestingData/model${k}/matches/${file_number1}_${file_number2}_0_1.npz" --output="PartialDeformedData/TestingData/model${k}/output/${file_number1}_${file_number2}/${file_number1}_${file_number2}_0_1.ply" --output_trans="PartialDeformedData/TestingData/model${k}/output/${file_number1}_${file_number2}/${file_number1}_${file_number2}_0_1_se4.h5" --intermediate_ouput_folder="PartialDeformedData/TestingData/model${k}/output/${file_number1}_${file_number2}/"
-		python3 ../../code/sfm/python/graphics/mesh/compute_relative_transformation_error.py --part1="${base}/model${k}/transformed/${file_number1}_0_se4.h5" --part2="identity.h5" --pred="${base}/model${k}/output/${file_number1}_${file_number2}/${file_number1}_${file_number2}_0_1_se4.h5"
-        python3 ../../code/sfm/python/graphics/mesh/compute_pointcloud_rmse_ir.py --input1="${base}/model${k}/output/${file_number1}_${file_number2}/${file_number1}_${file_number2}_0_1.ply" --input2="PartialDeformedData/TestingData/model${k}/temp_${file_number1}/model_0/cloud/dense.ply"
+		python3 eval_supervised_astrivis.py --config=config/${config} --s="PartialDeformedData/TestingData/model${k}/transformed/${file_number1}_0.ply" --t="PartialDeformedData/TestingData/model${k}/temp_${file_number2}/model_1/cloud/dense.ply" --source_trans="PartialDeformedData/TestingData/model${k}/transformed/${file_number1}_0_se4.h5" --target_trans="identity.h5" --matches="PartialDeformedData/TestingData/model${k}/matches/${file_number1}_${file_number2}_0_1.npz" --output="PartialDeformedData/TestingData/model${k}/output/${file_number1}_${file_number2}/${file_number1}_${file_number2}_0_1.ply" --output_trans="PartialDeformedData/TestingData/model${k}/output/${file_number1}_${file_number2}/${file_number1}_${file_number2}_0_1_se4.h5" --intermediate_ouput_folder="PartialDeformedData/TestingData/model${k}/output/${file_number1}_${file_number2}/" >> ${filename}
+		python3 ../../code/sfm/python/graphics/mesh/compute_relative_transformation_error.py --part1="${base}/model${k}/transformed/${file_number1}_0_se4.h5" --part2="identity.h5" --pred="${base}/model${k}/output/${file_number1}_${file_number2}/${file_number1}_${file_number2}_0_1_se4.h5" >> ${filename}
+        python3 ../../code/sfm/python/graphics/mesh/compute_pointcloud_rmse_ir.py --input1="${base}/model${k}/output/${file_number1}_${file_number2}/${file_number1}_${file_number2}_0_1.ply" --input2="PartialDeformedData/TestingData/model${k}/temp_${file_number1}/model_0/cloud/dense.ply" >> ${filename}
 
 		# 1 -> 0
 		touch ${base}/model${k}/output/${file_number1}_${file_number2}_1_0_se4.h5
 		
-		python3 eval_supervised_astrivis.py --config=config/LNDP.yaml --s="PartialDeformedData/TestingData/model${k}/transformed/${file_number1}_1.ply" --t="PartialDeformedData/TestingData/model${k}/temp_${file_number2}/model_0/cloud/dense.ply" --source_trans="PartialDeformedData/TestingData/model${k}/transformed/${file_number1}_1_se4.h5" --target_trans="identity.h5" --matches="PartialDeformedData/TestingData/model${k}/matches/${file_number1}_${file_number2}_1_0.npz" --output="PartialDeformedData/TestingData/model${k}/output/${file_number1}_${file_number2}/${file_number1}_${file_number2}_1_0.ply" --output_trans="PartialDeformedData/TestingData/model${k}/output/${file_number1}_${file_number2}/${file_number1}_${file_number2}_1_0_se4.h5" --intermediate_ouput_folder="PartialDeformedData/TestingData/model${k}/output/${file_number1}_${file_number2}/"
-		python3 ../../code/sfm/python/graphics/mesh/compute_relative_transformation_error.py --part1="${base}/model${k}/transformed/${file_number1}_1_se4.h5" --part2="identity.h5" --pred="${base}/model${k}/output/${file_number1}_${file_number2}/${file_number1}_${file_number2}_1_0_se4.h5"
-        python3 ../../code/sfm/python/graphics/mesh/compute_pointcloud_rmse_ir.py --input1="${base}/model${k}/output/${file_number1}_${file_number2}/${file_number1}_${file_number2}_1_0.ply" --input2="PartialDeformedData/TestingData/model${k}/temp_${file_number1}/model_1/cloud/dense.ply"
+		python3 eval_supervised_astrivis.py --config=config/${config} --s="PartialDeformedData/TestingData/model${k}/transformed/${file_number1}_1.ply" --t="PartialDeformedData/TestingData/model${k}/temp_${file_number2}/model_0/cloud/dense.ply" --source_trans="PartialDeformedData/TestingData/model${k}/transformed/${file_number1}_1_se4.h5" --target_trans="identity.h5" --matches="PartialDeformedData/TestingData/model${k}/matches/${file_number1}_${file_number2}_1_0.npz" --output="PartialDeformedData/TestingData/model${k}/output/${file_number1}_${file_number2}/${file_number1}_${file_number2}_1_0.ply" --output_trans="PartialDeformedData/TestingData/model${k}/output/${file_number1}_${file_number2}/${file_number1}_${file_number2}_1_0_se4.h5" --intermediate_ouput_folder="PartialDeformedData/TestingData/model${k}/output/${file_number1}_${file_number2}/" >> ${filename}
+		python3 ../../code/sfm/python/graphics/mesh/compute_relative_transformation_error.py --part1="${base}/model${k}/transformed/${file_number1}_1_se4.h5" --part2="identity.h5" --pred="${base}/model${k}/output/${file_number1}_${file_number2}/${file_number1}_${file_number2}_1_0_se4.h5" >> ${filename}
+        python3 ../../code/sfm/python/graphics/mesh/compute_pointcloud_rmse_ir.py --input1="${base}/model${k}/output/${file_number1}_${file_number2}/${file_number1}_${file_number2}_1_0.ply" --input2="PartialDeformedData/TestingData/model${k}/temp_${file_number1}/model_1/cloud/dense.ply" >> ${filename}
     done
 done
 
--- Testing/all/test_astrivis_partial_non_deformed.sh
+++ Testing/all/test_astrivis_partial_non_deformed.sh
@@ -1,3 +1,9 @@
+# config=LNDP_fcgf.yaml
+config=LNDP.yaml
+filename=Testing/all/test_astrivis_partial_deformed.txt
+rm ${filename}
+touch ${filename}
+
 base='/home/aiday.kyzy/dataset/Synthetic/PartialNonDeformedData/TestingData'
 
 model_numbers=('002' '008' '015' '022' '029' '035' '042' '049' '056' '066' '073' '079' '085' '093' '100' '106' '113' '120' '126' '133' '140' '147' '153' '160' '167' '174' '180' '187' '194' '201' '207' '214' '221')
@@ -5,12 +11,12 @@ for k in ${model_numbers[@]}
 do
 
     mkdir $base/model$k/output
-    echo "model ${k}"
+    echo "model ${k}" >> ${filename}
 
     # 0 -> 1
     touch ${base}/model${k}/output/0_1_se4.h5
-    python3 eval_supervised_astrivis.py --config=config/LNDP.yaml --s="PartialNonDeformedData/TestingData/model${k}/transformed/mesh_transformed_0.ply" --t="PartialNonDeformedData/TestingData/model${k}/temp/model_1/cloud/dense.ply" --source_trans="PartialNonDeformedData/TestingData/model${k}/transformed/mesh_transformed_0_se4.h5" --target_trans="identity.h5" --matches="PartialNonDeformedData/TestingData/model${k}/matches/0_1.npz" --output="PartialNonDeformedData/TestingData/model${k}/output/0_1.ply" --output_trans="PartialNonDeformedData/TestingData/model${k}/output/0_1_se4.h5" --intermediate_ouput_folder="PartialNonDeformedData/TestingData/model${k}/output/"
-    python3 ../../code/sfm/python/graphics/mesh/compute_relative_transformation_error.py --part1="${base}/model${k}/transformed/mesh_transformed_0_se4.h5" --part2="identity.h5" --pred="${base}/model${k}/output/0_1_se4.h5"
-    python3 ../../code/sfm/python/graphics/mesh/compute_pointcloud_rmse_ir.py --input1="${base}/model${k}/output/0_1.ply" --input2="${base}/model${k}/temp/model_0/cloud/dense.ply"
+    python3 eval_supervised_astrivis.py --config=config/${config} --s="PartialNonDeformedData/TestingData/model${k}/transformed/mesh_transformed_0.ply" --t="PartialNonDeformedData/TestingData/model${k}/temp/model_1/cloud/dense.ply" --source_trans="PartialNonDeformedData/TestingData/model${k}/transformed/mesh_transformed_0_se4.h5" --target_trans="identity.h5" --matches="PartialNonDeformedData/TestingData/model${k}/matches/0_1.npz" --output="PartialNonDeformedData/TestingData/model${k}/output/0_1.ply" --output_trans="PartialNonDeformedData/TestingData/model${k}/output/0_1_se4.h5" --intermediate_ouput_folder="PartialNonDeformedData/TestingData/model${k}/output/" >> ${filename}
+    python3 ../../code/sfm/python/graphics/mesh/compute_relative_transformation_error.py --part1="${base}/model${k}/transformed/mesh_transformed_0_se4.h5" --part2="identity.h5" --pred="${base}/model${k}/output/0_1_se4.h5" >> ${filename}
+    python3 ../../code/sfm/python/graphics/mesh/compute_pointcloud_rmse_ir.py --input1="${base}/model${k}/output/0_1.ply" --input2="${base}/model${k}/temp/model_0/cloud/dense.ply" >> ${filename}
 
 done
