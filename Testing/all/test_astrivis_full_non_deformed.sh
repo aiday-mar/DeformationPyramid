a2bc513e2ae5671ea0cddfbea755c0c82eed2cd4
aiday-mar <aidaymarlenkyzy@gmail.com>
2022-12-07 20:23:25 +0100

    considering 4 different data types

--- Testing/all/test_astrivis_full_non_deformed.sh
+++ Testing/all/test_astrivis_full_non_deformed.sh
@@ -1,8 +1,8 @@
-# config=LNDP_fcgf.yaml
-config=LNDP.yaml
+config=LNDP_fcgf.yaml
+# config=LNDP.yaml
 
-#type=fcgf
-type=kpfcn
+type=fcgf
+# type=kpfcn
 
 filename=Testing/all/test_astrivis_full_non_deformed_${type}.txt
 rm ${filename}
@@ -11,13 +11,14 @@ touch ${filename}
 base='/home/aiday.kyzy/dataset/Synthetic/FullNonDeformedData/TestingData'
 # model_numbers=('002' '008' '015' '022' '029' '035' '042' '049' '056' '066' '073' '079' '085' '093' '100' '106' '113' '120' '126' '133' '140' '147' '153' '160' '167' '174' '180' '187' '194' '201' '207' '214' '221')
 model_numbers=('002' '042' '085' '126' '167' '207')
+folder_name=output_full_non_deformed_${type}
 
 for k in ${model_numbers[@]}
 do
-
-    mkdir $base/model$k/output
-    touch ${base}/model${k}/output/0_1_se4.h5
     echo "model ${k}" >> ${filname}
+    mkdir $base/model$k/${folder_name}
+    touch ${base}/model${k}/${folder_name}/0_1_se4.h5
+    
     python3 eval_supervised_astrivis.py \
     --config=config/${config} \
     --s="FullNonDeformedData/TestingData/model${k}/mesh_transformed_0.ply" \
@@ -25,19 +26,19 @@ do
     --source_trans="FullNonDeformedData/TestingData/model${k}/mesh_transformed_0_se4.h5" \
     --target_trans="identity.h5" \
     --matches="FullNonDeformedData/TestingData/model${k}/0_1.npz" \
-    --output="FullNonDeformedData/TestingData/model${k}/output/0_1.ply" \
-    --output_trans="FullNonDeformedData/TestingData/model${k}/output/0_1_se4.h5" \
-    --intermediate_ouput_folder="FullNonDeformedData/TestingData/model${k}/output/" \
+    --output="FullNonDeformedData/TestingData/model${k}/${folder_name}/0_1.ply" \
+    --output_trans="FullNonDeformedData/TestingData/model${k}/${folder_name}/0_1_se4.h5" \
+    --intermediate_ouput_folder="FullNonDeformedData/TestingData/model${k}/${folder_name}/" \
     --print_keypoints >> ${filname}
     
     if [ "$?" != "1" ]; then
     python3 ../../code/sfm/python/graphics/mesh/compute_relative_transformation_error.py \
     --part1="${base}/model${k}/mesh_transformed_0_se4.h5" \
     --part2="identity.h5" 
-    --pred="${base}/model${k}/output/0_1_se4.h5" >> ${filname}
+    --pred="${base}/model${k}/${folder_name}/0_1_se4.h5" >> ${filname}
     
     python3 ../../code/sfm/python/graphics/mesh/compute_pointcloud_rmse_ir.py \
-    --input1="${base}/model${k}/output/0_1.ply" \
+    --input1="${base}/model${k}/${folder_name}/0_1.ply" \
     --input2="${base}/model${k}/mesh_sampled.ply" \
     --matches="${base}/model${k}/0_1.npz" >> ${filname}
     fi
