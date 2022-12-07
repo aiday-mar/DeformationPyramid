9a59b8d3f706271d3e203c166cdb411f02772db8
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-12-07 13:11:57 +0100

    adding the value y into the title in order to icnrease the space between the title and the plot in the plot

--- Testing/confidence_threshold/aggregate_data_confidence_threshold_true_correspondence_ratio.py
+++ Testing/confidence_threshold/aggregate_data_confidence_threshold_true_correspondence_ratio.py
@@ -64,7 +64,7 @@ for data_type in data_types:
     # true_data = []
     # total_data = []
     
-    plt.title('Varying the confidence threshold - ' + data_type + ' -  ' + title)
+    plt.title('Varying the confidence threshold - ' + data_type + ' -  ' + title, y=1.12)
     
     for model in models:
         fraction = []
