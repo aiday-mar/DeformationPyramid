997e474fa7bdc8be2b74276b72feb4a1c00145c4
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-12-08 14:25:15 +0100

    adding code in order to aggregate the data for varyign positional encodings

--- Testing/posenc_function/aggregate_data_posenc_functions_rmse.py
+++ Testing/posenc_function/aggregate_data_posenc_functions_rmse.py
@@ -1,65 +1,72 @@
 import re
 import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
+import copy
 
-class File:
-    def __init__(self, type, preprocessing, confidence, number_centers, average_distance_multiplier, coarse_level, index_coarse, number_iterations, inlier_outlier_thr, sampling):
-        self.type = type
-        self.preprocessing = preprocessing
-        self.confidence = confidence
-        self.number_centers = number_centers
-        self.average_distance_multiplier = average_distance_multiplier
-        self.coarse_level = coarse_level
-        self.index_coarse = index_coarse
-        self.number_iterations = number_iterations
-        self.inlier_outlier_thr = inlier_outlier_thr
-        self.sampling = sampling
-    
-    def __str__(self):
-        return " - Type : " + str(self.type) + " - Preprocessing : " + str(self.preprocessing) + " - Confidence : " + str(self.confidence) + " - Number Centers : " + str(self.number_centers) + " - Average Distance Multiplier : " + str(self.average_distance_multiplier) + " - Coarse Level : " + str(self.coarse_level) + " - Index Coarse : " + str(self.index_coarse) + ' - Number Iterations : ' + str(self.number_iterations) + ' - Inlier Outlier Threshold : ' + str(self.inlier_outlier_thr) + ' - Sampling : ' + str(self.sampling)
+# feature_extractor='kpfcn'
+feature_extractor='fcgf' # DONE
 
-files=[]
-file_types=[]
-number_iterations=1
 data_types=['Full Non Deformed', 'Full Deformed', 'Partial Deformed', 'Partial Non Deformed']
 base = 'Testing/'
 folder = 'posenc_function/'
-file='testing_posenc_functions.txt'
-title = 'RMSE - Varying posenc functions'
+file='testing_posenc_functions_' + feature_extractor + '.txt'
 
-posenc_functions = ['none', 'power4', 'log', 'linear', 'square']
-shape = (len(posenc_functions),)
+posenc_functions_list=['linear' 'none' 'power4' 'log' 'square']
+models=['002', '042', '085', '126', '167', '207']
+shape = (len(posenc_functions_list),)
 
-final_matrices={'Full Non Deformed': {'rmse' : np.zeros(shape)}, 
+sub_matrix={'Full Non Deformed': {'rmse' : np.zeros(shape)}, 
                 'Full Deformed': {'rmse' : np.zeros(shape)}, 
                 'Partial Deformed': {'rmse' : np.zeros(shape)},  
                 'Partial Non Deformed': {'rmse' : np.zeros(shape)}}
+final_matrices = {model : copy.deepcopy(sub_matrix) for model in models}
 
 file_txt = open(base + folder + file, 'r')
 Lines = file_txt.readlines()
 posenc_function_val = ''
 current_data_type = ''
+current_model = None
 for line in Lines:
+    if 'model ' in line:
+        current_model = re.findall(r'\b\d+\b',line)[0]
+        if current_model not in models:
+            current_model = None
+            
+    if 'Test - positional encoding' in line:
+        if 'linear' in line:
+            posenc_function_val = 'linear'
+        elif 'none' in line:
+            posenc_function_val = 'none'
+        elif 'power4' in line:
+            posenc_function_val = 'power4'
+        elif 'log' in line:
+            posenc_function_val = 'log'
+        elif 'square' in line:
+            posenc_function_val = 'square'
+            
     if line[:-1] in data_types:
         current_data_type = line[:-1]
-    if 'Test - positional encoding' in line:
-        interm_list = line.split()
-        posenc_function_val = interm_list[-1]
-        if ',' in posenc_function_val:
-            posenc_function_val = posenc_function_val[:-1]
-    if 'RMSE' in line:
+        
+    if 'RMSE' in line and current_model is not None:
         rmse = list(map(float, re.findall("\d+\.\d+", line)))[0]
-        i = posenc_functions.index(posenc_function_val)
-        final_matrices[current_data_type]['rmse'][i] = rmse
+        i = posenc_functions_list.index(posenc_function_val)
+        final_matrices[current_model][current_data_type]['rmse'][i] = rmse
         
 print('final_matrices : ', final_matrices)
 
 for data_type in data_types:
     plt.clf()
-    posenc_functions_pos = range(len(posenc_functions))
-    plt.clf()
-    plt.title(title)
-    plt.plot(posenc_functions_pos, final_matrices[data_type]['rmse'], color='r')
-    plt.xticks(posenc_functions_pos, posenc_functions, rotation=90)
-    plt.savefig(base + folder + data_type.replace(' ', '_') + '_graph.png', bbox_inches='tight')
+    for model in models:
+        k0_pos = range(len(posenc_functions_list))
+        plt.plot(k0_pos, final_matrices[model][data_type]['rmse'])
+        plt.xticks(k0_pos, posenc_functions_list, rotation=90)
+    
+    if feature_extractor == 'fcgf':
+        plt.title('Varying positional encoding - RMSE - FCGF')
+    elif feature_extractor == 'kpfcn':
+        plt.title('Varying positional encoding - RMSE - KPFCN')
+        
+    plt.ylabel('RMSE')
+    plt.xlabel('positional encoding')
+    plt.legend(models, loc = "upper right")
+    plt.savefig(base + folder + data_type.replace(' ', '_') + '_graph_' + feature_extractor + '.png', bbox_inches='tight')
