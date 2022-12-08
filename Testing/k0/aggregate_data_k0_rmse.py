be34a759bf7b92de0da0f2e916bc87820ef767ad
aiday.kyzy <aiday.kyzy@astrivis.com>
2022-12-08 12:56:42 +0100

    setting different titles depending on the fcgf or kpfcn values

--- Testing/k0/aggregate_data_k0_rmse.py
+++ Testing/k0/aggregate_data_k0_rmse.py
@@ -9,7 +9,6 @@ data_types=['Full Non Deformed', 'Full Deformed', 'Partial Deformed', 'Partial N
 base = 'Testing/'
 folder = 'k0/'
 file='testing_k0_' + feature_extractor + '.txt'
-title = 'Varying k0 - RMSE - ' + feature_extractor
 
 k0_list=[-11, -10, -9, -8]
 models=['002', '042', '085', '126', '167', '207']
@@ -31,20 +30,15 @@ for line in Lines:
         current_model = re.findall(r'\b\d+\b',line)[0]
         if current_model not in models:
             current_model = None
-        else:
-            print(current_model)
             
     if 'Test - k0' in line:
         k0_val = int(re.findall('-?\d+', line)[1])
-        print(k0_val)
         
     if line[:-1] in data_types:
         current_data_type = line[:-1]
-        print(current_data_type)
         
     if 'RMSE' in line and current_model is not None:
         rmse = list(map(float, re.findall("\d+\.\d+", line)))[0]
-        print(rmse)
         i = k0_list.index(k0_val)
         final_matrices[current_model][current_data_type]['rmse'][i] = rmse
         
@@ -54,8 +48,14 @@ for data_type in data_types:
     plt.clf()
     for model in models:
         k0_pos = range(len(k0_list))
-        plt.title(title)
         plt.plot(k0_pos, final_matrices[model][data_type]['rmse'], color='r')
         plt.xticks(k0_pos, k0_list, rotation=90)
     
+    if feature_extractor == 'fcgf':
+        plt.title('Varying k0 - RMSE - FCGF')
+    elif feature_extractor == 'kpfcn':
+        plt.title('Varying k0 - RMSE - KPFCN')
+        
+    plt.ylabel('RMSE')
+    plt.xlabel('k0')
     plt.savefig(base + folder + data_type.replace(' ', '_') + '_graph_' + feature_extractor + '.png', bbox_inches='tight')
