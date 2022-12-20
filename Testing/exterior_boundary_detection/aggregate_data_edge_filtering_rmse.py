fddbf5000ffb2e650e64a3eea75f6e57ad5c3ddd
aiday-mar <aidaymarlenkyzy@gmail.com>
2022-12-20 14:24:07 +0100

    adding two plots for the rmse and ir in the case of kpfcn pretrained network

--- Testing/exterior_boundary_detection/aggregate_data_edge_filtering_rmse.py
+++ Testing/exterior_boundary_detection/aggregate_data_edge_filtering_rmse.py
@@ -6,98 +6,87 @@ import copy
 # FCGF TODO
 # KPFCN TODO
 
-# criterion='simple'
-criterion='angle'
-# criterion='shape'
-# criterion='disc'
-
 feature_extractor='kpfcn'
 # feature_extractor='fcgf'
 
 # preprocessing=none
 preprocessing='mutual'
 
+models=['002', '042', '085', '126', '167', '207']
+criteria = ['simple', 'angle', 'shape', 'disc', 'none']
 data_types=['Partial Deformed', 'Partial Non Deformed']
 base = 'Testing/'
 folder = 'exterior_boundary_detection/'
-file='testing_' + criterion + '_edge_filtering_pre_' + preprocessing + '_' + feature_extractor + '.txt'
 
-edge_filtering_list=['Edge filtering not used', 'Edge filtering used']
-models=['002', '042', '085', '126', '167', '207']
-
-shape = (len(edge_filtering_list),)
-
-sub_matrix={
-    'Partial Deformed': {'rmse' : np.zeros(shape), 'strict-ir' : np.zeros(shape), 'relaxed-ir' : np.zeros(shape), 'vis-epe' : np.zeros(shape), 'vis-outlier' :  np.zeros(shape)},  
-    'Partial Non Deformed': {'rmse' : np.zeros(shape), 'strict-ir' : np.zeros(shape), 'relaxed-ir' : np.zeros(shape), 'vis-epe' : np.zeros(shape), 'vis-outlier' :  np.zeros(shape)}
+sub_sub_matrix = {'rmse' : 0., 'strict-ir' : 0., 'relaxed-ir' : 0., 'vis-epe' : 0., 'vis-outlier' :  0.}
+sub_matrix = {
+    'Partial Deformed': {criterion : copy.deepcopy(sub_sub_matrix) for criterion in criteria},  
+    'Partial Non Deformed': {criterion : copy.deepcopy(sub_sub_matrix) for criterion in criteria}
 }
 final_matrices = {model : copy.deepcopy(sub_matrix) for model in models}
 
-file_txt = open(base + folder + file, 'r')
-Lines = file_txt.readlines()
-current_data_type = ''
-current_model = None
-edge_filtering_val = None
+for criterion in criteria:
+    file='testing_' + criterion + '_edge_filtering_pre_' + preprocessing + '_' + feature_extractor + '.txt'
+    file_txt = open(base + folder + file, 'r')
+    Lines = file_txt.readlines()
+    current_data_type = ''
+    current_model = None
+    criterion_val = None
 
-for line in Lines:
-    if 'model ' in line:
-        current_model = re.findall(r'\b\d+\b',line)[0]
-        if current_model not in models:
-            current_model = None
+    for line in Lines:
+        if 'model ' in line:
+            current_model = re.findall(r'\b\d+\b',line)[0]
+                
+        if 'using angle exterior boundary detection' in line:
+            criterion_val = 'angle'
+
+        if 'using disc exterior boundary detection' in line:
+            criterion_val = 'disc'
+
+        if 'using shape exterior boundary detection' in line:
+            criterion_val = 'shape'
+        
+        if 'using simple exterior boundary detection' in line:
+            criterion_val = 'simple'
+
+        if 'Edge filtering not used' in line:
+            criterion_val = 'none'
+        
+        if line[:-1] in data_types:
+            current_data_type = line[:-1]
             
-    if 'Edge filtering not used' in line:
-        edge_filtering_val = 'Edge filtering not used'
-    elif 'Edge filtering used' in line:
-        edge_filtering_val = 'Edge filtering used'
-              
-    if line[:-1] in data_types:
-        current_data_type = line[:-1]
-        
-    if 'RMSE' in line and current_model is not None and edge_filtering_val is not None:
-        rmse = list(map(float, re.findall("\d+\.\d+", line)))[0]
-        i = edge_filtering_list.index(edge_filtering_val)
-        final_matrices[current_model][current_data_type]['rmse'][i] = rmse
+        if 'RMSE' in line and current_model is not None and criterion_val is not None:
+            rmse = list(map(float, re.findall("\d+\.\d+", line)))[0]
+            final_matrices[current_model][current_data_type][criterion_val]['rmse'] = rmse
 
-    if 'Strict IR' in line and current_model is not None and edge_filtering_val is not None:
-        strict_ir = list(map(float, re.findall("\d+\.\d+", line)))[0]
-        i = edge_filtering_list.index(edge_filtering_val)
-        final_matrices[current_model][current_data_type]['strict-ir'][i] = strict_ir
+        if 'Strict IR' in line and current_model is not None and criterion_val is not None:
+            strict_ir = list(map(float, re.findall("\d+\.\d+", line)))[0]
+            final_matrices[current_model][current_data_type][criterion_val]['strict-ir'] = strict_ir
 
-    if 'Relaxed IR' in line and current_model is not None and edge_filtering_val is not None:
-        relaxed_ir = list(map(float, re.findall("\d+\.\d+", line)))[0]
-        i = edge_filtering_list.index(edge_filtering_val)
-        final_matrices[current_model][current_data_type]['relaxed-ir'][i] = relaxed_ir
-        
-    if 'vis-epe' in line and current_model is not None and edge_filtering_val is not None:
-        vis_epe = list(map(float, re.findall("\d+\.\d+", line)))[0]
-        i = edge_filtering_list.index(edge_filtering_val)
-        final_matrices[current_model][current_data_type]['vis-epe'][i] = vis_epe
-        
-    if 'vis-outlier' in line and current_model is not None and edge_filtering_val is not None:
-        vis_outlier = list(map(float, re.findall("\d+\.\d+", line)))[0]
-        i = edge_filtering_list.index(edge_filtering_val)
-        final_matrices[current_model][current_data_type]['vis-outlier'][i] = vis_outlier
-        
-print('final_matrices : ', final_matrices)
-
+        if 'Relaxed IR' in line and current_model is not None and criterion_val is not None:
+            relaxed_ir = list(map(float, re.findall("\d+\.\d+", line)))[0]
+            final_matrices[current_model][current_data_type][criterion_val]['relaxed-ir'] = relaxed_ir
+            
+        if 'vis-epe' in line and current_model is not None and criterion_val is not None:
+            vis_epe = list(map(float, re.findall("\d+\.\d+", line)))[0]
+            final_matrices[current_model][current_data_type][criterion_val]['vis-epe'] = vis_epe
+            
+        if 'vis-outlier' in line and current_model is not None and criterion_val is not None:
+            vis_outlier = list(map(float, re.findall("\d+\.\d+", line)))[0]
+            final_matrices[current_model][current_data_type][criterion_val]['vis-outlier'] = vis_outlier
+            
 for data_type in data_types:
     plt.clf()
-    rmse_no_edge_filtering = []
-    rmse_edge_filtering = []
+    for criterion in criteria:
+        criterion_res = []
+
+        for model_number in models:
+            criterion_res.append(final_matrices[model_number][data_type][criterion]['rmse'])
+        
+        plt.plot(models, criterion_res)
     
-    for model in models:
-        rmse_no_edge_filtering.append(final_matrices[model][data_type]['rmse'][0])
-        rmse_edge_filtering.append(final_matrices[model][data_type]['rmse'][1])
-    
-    print(rmse_no_edge_filtering)
-    print(rmse_edge_filtering)
-    plt.plot(models, rmse_no_edge_filtering)
-    plt.plot(models, rmse_edge_filtering)
-    plt.xlabel('model')
+    plt.title(data_type)
     plt.ylabel('RMSE')
-    plt.legend(['No Edge Filtering', 'Edge Filtering'])
-    if feature_extractor == 'fcgf':
-        plt.title(data_type + ' - edge filtering comparison - ' + 'FCGF feature extraction')
-    elif feature_extractor == 'kpfcn':
-        plt.title(data_type + ' - edge filtering comparison - ' + 'KPFCN feature extraction')
-    plt.savefig(base + folder + data_type.replace(' ', '_') + '_graph_' + feature_extractor + '_' + criterion + '.png', bbox_inches='tight')
+    plt.xlabel('model numbers')
+    plt.legend(criteria, loc = "upper right")
+    plt.savefig(base + folder + data_type.replace(' ', '_') + '_rmse_' + feature_extractor + '.png', bbox_inches='tight')
