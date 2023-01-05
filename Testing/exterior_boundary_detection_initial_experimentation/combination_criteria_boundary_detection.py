a94617e0720f6304d12eda3cffbabaebaa6f0620
aiday-mar <aidaymarlenkyzy@gmail.com>
2023-01-05 22:58:49 +0100

    adding the simple criterion as an initial experimentation too

--- Testing/exterior_boundary_detection_initial_experimentation/combination_criteria_boundary_detection.py
+++ Testing/exterior_boundary_detection_initial_experimentation/combination_criteria_boundary_detection.py
@@ -1,6 +1,7 @@
 from angle_criterion_boundary_detection import get_angle_criterion_mask 
 from shape_criterion_boundary_detection import get_shape_criterion_mask
-from half_disc_criterion_boundary_detection import get_half_disc_criterion_mask 
+from half_disc_criterion_boundary_detection import get_half_disc_criterion_mask
+from simple_boundary_detection import apply_simple_boundary_detection
 import open3d as o3d
 import numpy as np
 
@@ -21,23 +22,29 @@ def apply_different_criteria(file_path, num):
 file_path = 'TestData/PartialDeformed/model002/020_0.ply'
 num = '020'
 apply_different_criteria(file_path, num)
+apply_simple_boundary_detection(file_path, num)
 
 file_path = 'TestData/PartialDeformed/model042/020_0.ply'
 num = '042'
 apply_different_criteria(file_path, num)
+apply_simple_boundary_detection(file_path, num)
 
 file_path = 'TestData/PartialDeformed/model085/020_0.ply'
 num = '085'
 apply_different_criteria(file_path, num)
+apply_simple_boundary_detection(file_path, num)
 
 file_path = 'TestData/PartialDeformed/model126/020_0.ply'
 num = '126'
 apply_different_criteria(file_path, num)
+apply_simple_boundary_detection(file_path, num)
 
 file_path = 'TestData/PartialDeformed/model167/020_0.ply'
 num = '167'
 apply_different_criteria(file_path, num)
+apply_simple_boundary_detection(file_path, num)
 
 file_path = 'TestData/PartialDeformed/model207/020_0.ply'
 num = '207'
 apply_different_criteria(file_path, num)
+apply_simple_boundary_detection(file_path, num)
--- Testing/exterior_boundary_detection_initial_experimentation/simple_boundary_detection.py
+++ Testing/exterior_boundary_detection_initial_experimentation/simple_boundary_detection.py
@@ -1,18 +1,31 @@
-src_pcd_points = data['src_pcd_list'][0]
-n_src_points = src_pcd_points.shape[0]
-dists = np.zeros((n_src_points, n_src_points))
-for i in range(n_src_points):
-    src_point = src_pcd_points[i]
-    difference = src_pcd_points - src_point
-    square_difference = difference ** 2
-    square_difference = np.array(square_difference.cpu())
-    sum_by_row = np.sum(square_difference, axis=1)
-    dists[i, :] = np.sqrt(sum_by_row)
+import numpy as np
+from scipy.spatial import distance_matrix
+import math 
+import open3d as o3d
 
-for i in range(n_src_points):
-    dists[i][i] = float('inf')
-min_distances = dists.min(axis = 1)
-average_distance = np.average(min_distances)
-neighbors = dists < 1.5 * average_distance
-n_neighbors = np.sum(neighbors, axis=1)
-initial_edge_point_indices = np.argwhere(n_neighbors < 3)
+def apply_simple_boundary_detection(file_path, num):
+    pcd = o3d.io.read_point_cloud(file_path)
+    pcd_points = np.array(pcd.points)
+    n_pcd_points = pcd_points.shape[0]
+    dists = np.zeros((pcd_points, pcd_points))
+
+    for i in range(n_pcd_points):
+        src_point = pcd_points[i]
+        difference = pcd_points - src_point
+        square_difference = difference ** 2
+        square_difference = np.array(square_difference.cpu())
+        sum_by_row = np.sum(square_difference, axis=1)
+        dists[i, :] = np.sqrt(sum_by_row)
+
+    for i in range(n_pcd_points):
+        dists[i][i] = float('inf')
+
+    min_distances = dists.min(axis = 1)
+    average_distance = np.average(min_distances)
+    neighbors = dists < 1.5 * average_distance
+    n_neighbors = np.sum(neighbors, axis=1)
+    initial_edge_point_indices = np.argwhere(n_neighbors < 3)
+    edge_points = pcd_points[initial_edge_point_indices]
+    edge_points_pcd = o3d.geometry.PointCloud()
+    edge_points_pcd.points = o3d.utility.Vector3dVector(np.array(edge_points))
+    o3d.io.write_point_cloud('Testing/exterior_boundary_detection/simple_criterion_' + num + '.ply', edge_points_pcd)
