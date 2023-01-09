3cd9029a9b060c44e94f359270157a239d2a3096
aiday-mar <aidaymarlenkyzy@gmail.com>
2023-01-09 16:04:55 +0100

    adding mesh criteria

--- Testing/exterior_boundary_detection_initial_experimentation/mesh_boundary_detection.py
+++ Testing/exterior_boundary_detection_initial_experimentation/mesh_boundary_detection.py
@@ -2,11 +2,23 @@ import open3d as o3d
 import numpy as np
 from collections import defaultdict
 
+surface_reconstruction='ball_pivoting'
+# surface_reconstruction='poisson'
+# surface_reconstruction='alpha'
+
 def mesh_criterion_boundary_detection(filename, num):
     pcd = o3d.io.read_point_cloud(filename)
-    radii = [0.01, 0.02, 0.04]
-    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd, o3d.utility.DoubleVector(radii))
-    o3d.io.write_triangle_mesh('Testing/exterior_boundary_detection_initial_experimentation/model' + num + '_mesh.ply', mesh)
+    if surface_reconstruction=='ball_pivoting':
+        radii = [0.01, 0.02, 0.04]
+        mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd, o3d.utility.DoubleVector(radii))
+    elif surface_reconstruction=='poisson':
+        mesh, _ = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=9)
+    elif surface_reconstruction=='alpha':
+        alpha = 0.05
+        mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, alpha)
+    else:
+        raise Exception('Specify a valid reconstruction method')
+    o3d.io.write_triangle_mesh('Testing/exterior_boundary_detection_initial_experimentation/model' + num + '_mesh_' + surface_reconstruction + '.ply', mesh)
     print(mesh)
     mesh_vertices = np.asarray(mesh.vertices)
     mesh_triangles = np.asarray(mesh.triangles)
@@ -45,8 +57,9 @@ def mesh_criterion_boundary_detection(filename, num):
     edge_vertices = mesh_vertices[edge_vertices]
     final_pcd = o3d.geometry.PointCloud()
     final_pcd.points = o3d.utility.Vector3dVector(edge_vertices)
-    o3d.io.write_point_cloud('Testing/exterior_boundary_detection_initial_experimentation/mesh_criterion_' + num + '.ply', final_pcd)
+    o3d.io.write_point_cloud('Testing/exterior_boundary_detection_initial_experimentation/mesh_criterion_' + num + '_' + surface_reconstruction + '.ply', final_pcd)
 
+'''
 filename='Testing/exterior_boundary_detection_initial_experimentation/model002.ply'
 num='002'
 mesh_criterion_boundary_detection(filename, num)
@@ -66,6 +79,7 @@ mesh_criterion_boundary_detection(filename, num)
 filename='Testing/exterior_boundary_detection_initial_experimentation/model167.ply'
 num='167'
 mesh_criterion_boundary_detection(filename, num)
+'''
 
 filename='Testing/exterior_boundary_detection_initial_experimentation/model207.ply'
 num='207'
