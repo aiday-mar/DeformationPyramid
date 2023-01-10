947a6ba18003fc5dc1cc7ae63cdb3a958659eb3a
aiday-mar <aidaymarlenkyzy@gmail.com>
2023-01-10 23:40:53 +0100

    model 042 text

--- Testing/w_cd_w_reg/aggregate_data_optimize_weights.py
+++ Testing/w_cd_w_reg/aggregate_data_optimize_weights.py
@@ -5,13 +5,14 @@ import copy
 
 w_reg_list = [0, 0.3, 0.5, 0.7, 1]
 w_cd_list = [0, 0.3, 0.5, 0.7, 1]
-types=['Full Deformed', 'Full Non Deformed', 'Partial Deformed', 'Partial Non Deformed']
+types=['Full Non Deformed', 'Full Deformed', 'Partial Deformed', 'Partial Non Deformed']
 matrix = np.zeros([len(w_reg_list), len(w_cd_list)])
 final_matrices = {type : copy.deepcopy(matrix) for type in types}
 w_reg_idx = 0
 w_cd_idx = 0
+model='207'
 
-file='Testing/w_cd_w_reg/testing_w_pre_mutual_kpfcn_td_pretrained.txt'
+file='Testing/w_cd_w_reg/testing_w_pre_mutual_kpfcn_td_pretrained_' + model + '.txt'
 f = open(file, "r")
 idx = 0
 for line in f:
@@ -58,6 +59,6 @@ for idx in range(len(types)):
     type = types[idx]
     ax.set_title(type)
     fig.tight_layout()
-    fig.savefig('Testing/w_cd_w_reg/' + types[idx].replace(' ', '_') + '_rmse.png')
+    fig.savefig('Testing/w_cd_w_reg/' + types[idx].replace(' ', '_') + '_rmse_' + model + '.png')
                    
     
