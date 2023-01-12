f00114155fe4be6df75b5ab382ad037b51568506
aiday-mar <aidaymarlenkyzy@gmail.com>
2023-01-12 19:56:40 +0100

    decreasing the confidence threshold more

--- Testing/current_deformation/test_astrivis_partial_non_deformed_current_deformation.sh
+++ Testing/current_deformation/test_astrivis_partial_non_deformed_current_deformation.sh
@@ -27,8 +27,8 @@ fi
 
 # further decreasing the confidence threshold
 if [ "$training_data" == "pretrained" ] ; then
-	confidence_threshold=0.01
-    confidence_threshold_name=0.01
+	confidence_threshold=0.001
+    confidence_threshold_name=0.001
 else
 	confidence_threshold=0.001
     confidence_threshold_name=1e-03
