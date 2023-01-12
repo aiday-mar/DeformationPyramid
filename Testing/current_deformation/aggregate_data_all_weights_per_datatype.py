9b5383c6a9eead5748bc3d39b9ae966778fec29a
aiday-mar <aidaymarlenkyzy@gmail.com>
2023-01-12 17:14:42 +0100

    using the kpfcn full deformed weights instead

--- Testing/current_deformation/aggregate_data_all_weights_per_datatype.py
+++ Testing/current_deformation/aggregate_data_all_weights_per_datatype.py
@@ -54,14 +54,14 @@ confidence_thresholds = {
         'kpfcn_pretrained' : '0.1', 
         'kpfcn_full_deformed' : '1e-06',
         'fcgf_full_deformed' : '1e-06',
-        'kpfcn_partial_deformed' : '1e-04',
+        'kpfcn_partial_deformed' : '1e-02',
         'fcgf_partial_deformed' : '1e-06'
     },
     'partial_non_deformed' : {
         'kpfcn_pretrained' : '0.1',
         'kpfcn_full_deformed' : '1e-06',
         'fcgf_full_deformed' : '1e-06',
-        'kpfcn_partial_deformed' : '1e-04',
+        'kpfcn_partial_deformed' : '1e-02',
         'fcgf_partial_deformed' : '1e-06'
     },
 }
