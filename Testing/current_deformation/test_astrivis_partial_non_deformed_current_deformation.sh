8eee59063413e6bb2a79e5cd28e419d862115ea8
aiday-mar <aidaymarlenkyzy@gmail.com>
2023-01-16 15:39:20 +0100

    making the conf bigger yet

--- Testing/current_deformation/test_astrivis_partial_non_deformed_current_deformation.sh
+++ Testing/current_deformation/test_astrivis_partial_non_deformed_current_deformation.sh
@@ -30,8 +30,8 @@ if [ "$training_data" == "pretrained" ] ; then
 	confidence_threshold=0.01
     confidence_threshold_name=1e-02
 else
-	confidence_threshold=0.01
-    confidence_threshold_name=1e-02
+	confidence_threshold=0.1
+    confidence_threshold_name=1e-01
 fi
 
 # model_numbers=('002' '042' '085' '126' '167' '207')
