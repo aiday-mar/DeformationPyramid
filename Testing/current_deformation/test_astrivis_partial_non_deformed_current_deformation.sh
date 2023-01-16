c91e0a2fc979fd84ad4e89e7afae8e0bbfc4e4ba
aiday-mar <aidaymarlenkyzy@gmail.com>
2023-01-16 15:37:28 +0100

    further increasing the confidence threshold

--- Testing/current_deformation/test_astrivis_partial_non_deformed_current_deformation.sh
+++ Testing/current_deformation/test_astrivis_partial_non_deformed_current_deformation.sh
@@ -30,8 +30,8 @@ if [ "$training_data" == "pretrained" ] ; then
 	confidence_threshold=0.01
     confidence_threshold_name=1e-02
 else
-	confidence_threshold=0.001
-    confidence_threshold_name=1e-03
+	confidence_threshold=0.01
+    confidence_threshold_name=1e-02
 fi
 
 # model_numbers=('002' '042' '085' '126' '167' '207')
