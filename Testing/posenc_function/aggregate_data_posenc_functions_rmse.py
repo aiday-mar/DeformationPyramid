90c74b637bf3df0ea3328e790f1e1b9872a7a387
aiday-mar <aidaymarlenkyzy@gmail.com>
2023-01-17 11:22:15 +0100

    changing the plots into bar plots

--- Testing/posenc_function/aggregate_data_posenc_functions_rmse.py
+++ Testing/posenc_function/aggregate_data_posenc_functions_rmse.py
@@ -9,6 +9,16 @@ import copy
 feature_extractor='kpfcn'
 # feature_extractor='fcgf'
 
+barWidth = 0.13
+barWidthPlot = 0.13
+br1 = np.array([0, 1, 2, 3, 4])
+br2 = np.array([x + barWidth for x in br1])
+br3 = np.array([x + barWidth for x in br2])
+br4 = np.array([x + barWidth for x in br3])
+br5 = np.array([x + barWidth for x in br4])
+br6 = np.array([x + barWidth for x in br5])
+final_bars = [br1, br2, br3, br4, br5, br6]
+
 training_data='pretrained'
 preprocessing='mutual'
 data_types=['Full Non Deformed', 'Full Deformed', 'Partial Deformed', 'Partial Non Deformed']
@@ -59,11 +69,16 @@ for line in Lines:
         
 for data_type in data_types:
     plt.clf()
+    index = 0
     for model in models:
+        bar = final_bars[index]
         posenc_function_pos = range(len(posenc_functions_list))
-        plt.plot(posenc_function_pos, final_matrices[model][data_type]['rmse'])
-        plt.xticks(posenc_function_pos, posenc_functions_list, rotation=90)
+        print(bar)
+        print( final_matrices[model][data_type]['rmse'])
+        plt.bar(bar, final_matrices[model][data_type]['rmse'], width=barWidthPlot)
+        index += 1
     
+    plt.xticks([r + barWidthPlot for r in range(len(posenc_functions_list))], posenc_functions_list)
     plt.title(data_type)   
     plt.ylabel('RMSE')
     plt.xlabel('positional encoding')
