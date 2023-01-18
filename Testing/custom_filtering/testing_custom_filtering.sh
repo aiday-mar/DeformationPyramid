05ceea95a758e5f4910dc90b225b28b88ee46468
aiday-mar <aidaymarlenkyzy@gmail.com>
2023-01-18 17:22:31 +0100

    removing too small number of centers

--- Testing/custom_filtering/testing_custom_filtering.sh
+++ Testing/custom_filtering/testing_custom_filtering.sh
@@ -46,7 +46,7 @@ index_coarse_feats=1
 # number_center=400
 # number_centers=(100 200 300)
 # number_centers=(50)
-number_centers=(5 10 30 50 100 300 500)
+number_centers=(10 50 100 300 500)
 
 # average_distance_multiplier=1
 # average_distance_multiplier=2
