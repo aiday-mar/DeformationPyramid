efe7e5e79699cd6b5cb71ec6b8ec4e40bcc3d2fc
aiday-mar <aidaymarlenkyzy@gmail.com>
2023-01-20 21:23:47 +0100

    using the mesh criterion instead

--- Testing/exterior_boundary_detection/aggregate_data_edge_filtering_ground_truth_correspondence_ratio.py
+++ Testing/exterior_boundary_detection/aggregate_data_edge_filtering_ground_truth_correspondence_ratio.py
@@ -4,6 +4,7 @@ import re
 import numpy as np
 
 criteria = ['none', 'simple', 'angle', 'shape', 'disc', 'mesh']
+data_types = ['Partial Deformed', 'Partial Non Deformed']
 model_numbers=['002', '042', '085', '126', '167', '207']
 
 feature_extractor='fcgf'
@@ -13,7 +14,8 @@ preprocessing='mutual'
 
 bar = np.array([0, 1, 2, 3, 4, 5])
 
-final_sub_sub_matrix = {'true' : 0, 'total' : 0, 'rmse' : 0.0}
+final_sub_sub_sub_matrix = {'true' : 0, 'total' : 0, 'rmse' : 0.0}
+final_sub_sub_matrix = {data_type : copy.deepcopy(final_sub_sub_sub_matrix) for data_type in data_types}
 final_submatrix = {model_number : copy.deepcopy(final_sub_sub_matrix) for model_number in model_numbers}
 final_matrices = {criterion : copy.deepcopy(final_submatrix) for criterion in criteria}
 
@@ -22,22 +24,29 @@ for criterion in criteria:
         file_txt = 'Testing/exterior_boundary_detection/testing_' + criterion + '_edge_filtering_pre_' + preprocessing + '_' + feature_extractor + '_td_' + training_data + '_epoch_' + epoch + '.txt'
         file_txt = open(file_txt, 'r')
         Lines = file_txt.readlines()
+        
+        current_model = None
+        current_data_type = None
 
         for line in Lines:
             if 'model' in line and len(line) < 100:
                 current_model = re.findall(r'\b\d+\b',line)[0]
             
+            for data_type in data_types:
+                if data_type in line:
+                    current_data_type = data_type
+
             line_to_check = 'number of true landmark correspondences returned from ' + criterion + ' edge filtering'
-            if line_to_check in line:
+            if line_to_check in line and current_data_type:
                 search = list(map(int, re.findall(r'\d+', line)))
                 true = int(search[0])
                 total = int(search[1])
-                final_matrices[criterion][model_number]['true'] = true
-                final_matrices[criterion][model_number]['total'] = total - true
+                final_matrices[criterion][model_number][current_data_type]['true'] = true
+                final_matrices[criterion][model_number][current_data_type]['total'] = total - true
           
-            if 'RMSE' in line:
+            if 'RMSE' in line and current_data_type:
                 rmse = float(re.findall("\d+\.\d+", line)[0])
-                final_matrices[criterion][model_number]['rmse'] = rmse
+                final_matrices[criterion][model_number][current_data_type]['rmse'] = rmse
 
 print(final_matrices)
 
@@ -45,13 +54,16 @@ for model_number in model_numbers:
 
     plt.clf()
 
-    count = 0
-    for criterion in criteria:
-        bar = bars[count]
-        plt.bar(bar, true_data, color='r')
-        plt.bar(bar, total_data, bottom=true_data, color='b')
-        count += 1
+    true = []
+    total = []
 
-    plt.title('Model ' + model_number + ' - ' + data_type)
-    plt.xticks(modified_nc_pos, modified_nc, rotation=90)
-    plt.savefig('Testing/exterior_boundary_detection/')
+    for criterion in criteria:
+        true.append(final_matrices[criterion][model_number]['true'])
+        total.append(final_matrices[criterion][model_number]['total'])
+
+    plt.bar(bar, true, color='r')
+    plt.bar(bar, total, bottom=true, color='b')
+
+    plt.title('Model ' + model_number)
+    plt.xticks(bar, criteria, rotation=90)
+    plt.savefig('Testing/exterior_boundary_detection/ground_truth_correspondence_ratio_model_' + model_number + '.png')
