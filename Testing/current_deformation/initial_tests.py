d5bb9000417ea4257feae875e701087754631249
aiday-mar <aidaymarlenkyzy@gmail.com>
2023-01-21 23:58:34 +0100

    using the kpfcn feature extractor with the current deformation and the none criterion

--- Testing/current_deformation/initial_tests.py
+++ Testing/current_deformation/initial_tests.py
@@ -1,3 +1,7 @@
+import copy
+import re
+import numpy as np
+import matplotlib.pyplot as plt
 
 # initial tests using FCGF, KNN matching and Deformation Algorithm From Astrivis
 
@@ -9,22 +13,37 @@ epoch='10'
 
 data_types = ['Full Non Deformed', 'Full Deformed', 'Partial Non Deformed', 'Partial Deformed']
 
-final_data = {}
+model_numbers = ['002', '042', '085', '126', '167', '207']
+
+sub_matrix = {model_number : 0 for model_number in model_numbers}
+final_data = {data_type : copy.deepcopy(sub_matrix) for data_type in data_types}
+
+bar = np.array([0, 1, 2, 3, 4, 5])
 
 for data_type in data_types:
     data_type_mod = data_type.lower().replace(' ', '_')
-    file_name = 'test_astrivis_' + data_type_mod + '_current_deformation_pre_none_fcgf_td_' + training_data + '_e_' + epoch + '_knn_True_conf_1e-06.txt'
+    file_name = 'Testing/current_deformation/test_astrivis_' + data_type_mod + '_current_deformation_pre_none_fcgf_td_' + training_data + '_e_' + epoch + '_knn_True_conf_1e-06.txt'
 
     file = open(file_name, 'r')
     lines = file.readlines()
-    list_keywords = ['full-epe', 'full-AccR', 'full-AccS', 'full-outlier', 'vis-epe', 'vis-AccS', 'vis-AccR', 'vis-outlier', 'RMSE', 'Relaxed IR', 'Strict IR']
-    final_data = {}
-    key = None
+    current_model_number = None 
 
     for line in lines:
         if 'model' in line and len(line) < 35:
-            if data:
-                final_data[key] = data
-            
             words = line.split(' ')
-            model_number = words[1]
+            current_model_number = words[1]
+            current_model_number = current_model_number[:len(current_model_number)-1]
+        
+        if 'RMSE' in line and current_model_number:
+            list_res = re.findall("\d+\.\d+", line)
+            rmse = list_res[0]
+            final_data[data_type][current_model_number] = rmse
+
+    rmse_arr = []
+    for model_number in model_numbers:
+        rmse_arr.append(final_data[data_type][model_number])
+
+    print(rmse_arr)
+    plt.clf()
+    plt.bar(bar, rmse_arr, width = 1)
+    plt.savefig('Testing/current_deformation/initial_test_' + data_type_mod + '_fcgf_td_' + training_data + '.png')
